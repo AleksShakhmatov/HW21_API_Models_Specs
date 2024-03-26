7ffd48ae4c7c369573588362c22da74e0569f732
AShakhmatov <Coluchy@yandex.ru>
2024-03-27 00:47:00 +0600

    added models and specification

---
+++ src/test/java/models/lombok/UserDataLombokModel.java
@@ -0,0 +1,7 @@
+import lombok.Data;
+
+@Data
+public class SingleResourceLombokModel {
+    Integer id, year;
+    String name, color, pantone_value, url, text;
+}
---
+++ src/test/java/models/lombok/UserDataRegisterLombokModel.java
@@ -0,0 +1,8 @@
+package models.lombok;
+
+import lombok.Data;
+
+@Data
+public class UserDataLombokModel {
+    String name, job;
+}
